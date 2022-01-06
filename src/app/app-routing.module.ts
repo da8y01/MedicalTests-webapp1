a184768c74b3fbc5b501ead950a0a5ccba8398ce
LocalDev <dev@local.host>
2022-01-05 23:39:31 -0500

    Routes and updates to deploy to heroku.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { PatientComponent } from './patient/patient.component';
+import { MedicComponent } from './medic/medic.component';
+import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  { path: 'patient', component: PatientComponent },
+  { path: 'medic', component: MedicComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '',   redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
