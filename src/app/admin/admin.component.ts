8ce798129ba55e7260ef82ea488b532088818747
Developer <developer@local.host>
2022-03-22 21:47:02 -0500

    Fix pointer hand for buttons on Admin operations.

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -54,7 +54,8 @@ export class AdminComponent implements OnInit {
     this.deleteMedics = deleteList;
   }
 
-  deleteSelected(type: string) {
+  deleteSelected(type: string, event: Event) {
+    event.preventDefault()
     this.backendService
       .deleteUsers(type === 'patient' ? this.deletePatients : this.deleteMedics)
       .subscribe(
@@ -67,7 +68,8 @@ export class AdminComponent implements OnInit {
       );
   }
 
-  updatePatient() {
+  updatePatient(event: Event) {
+    event.preventDefault()
     if (this.deletePatients.length === 1) {
       this.router.navigate(['/', 'update-patient', this.deletePatients[0]]);
     } else {
@@ -75,7 +77,8 @@ export class AdminComponent implements OnInit {
     }
   }
 
-  updateMedic() {
+  updateMedic(event: Event) {
+    event.preventDefault()
     if (this.deleteMedics.length === 1) {
       this.router.navigate(['/', 'update-medic', this.deleteMedics[0]]);
     } else {
