8986594c3693ac58487976aa1cf9aeddf7e0b56b
Developer <developer@local.host>
2022-03-23 23:45:38 -0500

    Dynamic FormArray for exams on create patient.

--- src/app/create-patient/create-patient.component.ts
+++ src/app/create-patient/create-patient.component.ts
@@ -1,19 +1,11 @@
 import { HttpEventType } from '@angular/common/http';
 import { Component, ElementRef, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormArray, FormBuilder } from '@angular/forms';
 import { Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { finalize } from 'rxjs/operators';
 import { BackendService } from '../backend.service';
-// import { FileUploader, FileSelectDirective } from 'ng2-file-upload';
-// import { FileUploader } from 'ng2-file-upload';
-// import {  FileUploader } from 'ng2-file-upload/ng2-file-upload';
-// import { environment } from 'src/environments/environment';
-
-// const URL = '/api/';
-// const URL = `${environment.apiUrl}/results/upload`;
-// const URL = 'https://evening-anchorage-3159.herokuapp.com/api/';
 
 @Component({
   selector: 'app-create-patient',
@@ -31,70 +23,21 @@ export class CreatePatientComponent implements OnInit {
     address: [''],
     phone: [''],
     email: ['', Validators.required],
-    // fileUpload: ['', Validators.required],
+    examsArray: this.fb.array([]),
   });
   messages = '';
   fileName = '';
   uploadProgress!: number;
   uploadSub!: Subscription;
-  // uploadUrl = `${environment.apiUrl}/results/upload`
-  // public uploader:FileUploader = new FileUploader({url: this.uploadUrl});
-  // uploader:FileUploader;
-  // hasBaseDropZoneOver:boolean;
-  // hasAnotherDropZoneOver:boolean;
-  // response:string;
-  // declare a property called fileuploader and assign it to an instance of a new fileUploader.
-  // pass in the Url to be uploaded to, and pass the itemAlias, which would be the name of the //file input when sending the post request.
-  // public uploader: FileUploader = new FileUploader({
-  //   url: URL,
-  //   itemAlias: 'result',
-  // });
 
   constructor(
     private fb: FormBuilder,
     private backendService: BackendService,
     private el: ElementRef,
     private router: Router
-  ) {
-    // this.uploader = new FileUploader({
-    //   url: URL,
-    //   disableMultipart: true, // 'DisableMultipart' must be 'true' for formatDataFunction to be called.
-    //   formatDataFunctionIsAsync: true,
-    //   formatDataFunction: async (item: any) => {
-    //     console.info(item)
-    //     return new Promise( (resolve, reject) => {
-    //       resolve({
-    //         name: item._file.name,
-    //         length: item._file.size,
-    //         contentType: item._file.type,
-    //         date: new Date()
-    //       });
-    //     });
-    //   }
-    // });
-    // this.hasBaseDropZoneOver = false;
-    // this.hasAnotherDropZoneOver = false;
-    // this.response = '';
-    // this.uploader.response.subscribe( res => this.response = res );
-  }
+  ) {}
 
-  ngOnInit(): void {
-    // // override the onAfterAddingfile property of the uploader so it doesn't authenticate with
-    // // credentials.
-    // this.uploader.onAfterAddingFile = (file) => {
-    //   file.withCredentials = false;
-    // };
-    // // overide the onCompleteItem property of the uploader so we are
-    // // able to deal with the server response.
-    // this.uploader.onCompleteItem = (
-    //   item: any,
-    //   response: any,
-    //   status: any,
-    //   headers: any
-    // ) => {
-    //   console.log('ImageUpload:uploaded:', item, status, response);
-    // };
-  }
+  ngOnInit(): void {}
 
   onSubmit() {
     // TODO: Use EventEmitter with form value
@@ -108,6 +51,21 @@ export class CreatePatientComponent implements OnInit {
         this.messages = error;
       }
     );
+  }
+
+  get examsList() {
+    return this.patientForm.get('examsArray') as FormArray;
+  }
+
+  addExam(event: Event) {
+    event.preventDefault()
+    const exam = {
+      name: [''],
+      result: [''],
+      reading: [''],
+      date: [''],
+    };
+    this.examsList.push(this.fb.group(exam));
   }
 
   updateProfile() {
@@ -146,48 +104,5 @@ export class CreatePatientComponent implements OnInit {
   reset() {
     this.uploadProgress = null;
     this.uploadSub = null;
-  }
-
-  // public fileOverBase(e:any):void {
-  //   this.hasBaseDropZoneOver = e;
-  // }
-
-  // public fileOverAnother(e:any):void {
-  //   this.hasAnotherDropZoneOver = e;
-  // }
-  // the function which handles the file upload without using a plugin.
-  upload() {
-    // locate the file element meant for the file upload.
-    let inputEl: HTMLInputElement =
-      this.el.nativeElement.querySelector('#fileUpload');
-    // get the total amount of files attached to the file input.
-    let fileCount: number = inputEl.files.length;
-    // create a new fromdata instance
-    let formData = new FormData();
-    // check if the filecount is greater than zero, to be sure a file was selected.
-    if (fileCount > 0) {
-      // a file was selected
-      // append the key name 'photo' with the first file in the element
-      formData.append('result', inputEl.files.item(0));
-      // call the angular http method
-      // this.http
-      // // post the form data to the url defined above and map the response. Then subscribe
-      // // to initiate the post. if you don't subscribe, angular wont post.
-      //   .post(URL, formData).map((res:Response) => res.json()).subscribe(
-      //     // map the success function and alert the response
-      //     (success) => {
-      //       alert(success._body);
-      //     },
-      //     (error) => alert(error)
-      //   )
-      this.backendService.uploadResult(formData).subscribe(
-        (res) => {
-          console.info(res);
-        },
-        (error) => {
-          console.error(error);
-        }
-      );
-    }
   }
 }
